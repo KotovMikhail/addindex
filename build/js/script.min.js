8ecfec15bac903316ea7a5dcf79c3da6c60f81bc
Mikhail <mihail_kotov_86@mail.ru>
2019-10-24 16:06:00 +0300

    небольшие правки

--- build/js/script.min.js
+++ build/js/script.min.js
@@ -2,16 +2,15 @@ const btnBurger = document.querySelector(`.btn--burger`);
 const body = document.body;
 
 const onBtnMenuOpened = () => {
-
   body.classList.toggle(`menu-opened`);
-
 };
 
 const onEscMenuClosed = (evt) => {
-  if (evt.keyCode === 27) {
+  if (evt.keyCode === 27 && body.classList.contains(`menu-opened`)) {
     body.classList.remove(`menu-opened`);
+    return;
   }
-}
+};
 
 document.addEventListener(`keydown`, onEscMenuClosed)
 
